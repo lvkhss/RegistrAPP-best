6a51966dae9cdce04bde3539305719017cf41b27
lvkhss <lukh.nunez@duocuc.cl>
2024-10-19 17:22:31 -0300

    aa

--- src/app/principal/principal.page.ts
+++ src/app/principal/principal.page.ts
@@ -16,6 +16,9 @@ export class PrincipalPage implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    this.username = user.username || 'Invitado';
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
