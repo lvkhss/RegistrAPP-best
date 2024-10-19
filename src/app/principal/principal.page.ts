17ffbe7e2f99813b32aaa32a353ee4e1d0199770
lvkhss <lukh.nunez@duocuc.cl>
2024-10-19 17:09:10 -0300

    lista clases y qr

--- src/app/principal/principal.page.ts
+++ src/app/principal/principal.page.ts
@@ -7,14 +7,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./principal.page.scss'],
 })
 export class PrincipalPage implements OnInit {
-
   nombre: string = "";
+  menuOpen: boolean = false;
+  selectedClass: string = '';
+  qrCodeData: string = '';
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.nombre = params['nombre'] || 'Invitado';
-    })
+    });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  seleccionarClase(clase: string) {
+    this.selectedClass = clase;
+    this.qrCodeData = `Asistencia para la clase de ${clase}`;
+    this.menuOpen = false; // Cierra el menú después de seleccionar una clase
   }
 }
