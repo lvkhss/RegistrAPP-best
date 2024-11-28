bd36efbb3beeafdc374d6ccca049fc7c614ef78b
hijo del uxiono <dieg.donosop@duocuc.cl>
2024-11-28 01:59:55 -0300

    commit adsf
    agregue form para crear clases siendo profesor

--- src/app/crear-clases/crear-clases.page.ts
+++ src/app/crear-clases/crear-clases.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-crear-clases',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-clases.page.scss'],
 })
 export class CrearClasesPage implements OnInit {
+  usuario: string = "";
+  selectedClass: string = '';
+  programacionButtonVisible: boolean = false;
+  userData: any = {};
+  //valores de formulario
+  curso: any = {
+    nombre: '',
+    sigla: '',
+    institucion: '',
+    descripcion: ''
+  };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.usuario = params['usuario'] ? params['usuario'] : 'Invitado';
+    });
   }
 
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Form Submitted', this.curso);
+      this.postCurso(this.curso);
+    }
+  }
+ 
+  postCurso(curso: any) {
+    const url = 'https://www.presenteprofe.cl/api/v1/cursos'; 
+    this.http.post(url, curso).pipe(
+      catchError((error) => {
+        console.error('Error al enviar el curso:', error);
+        return of(null); // devuelve null porsiaca
+      })
+    ).subscribe((response) => {
+      if (response) {
+        console.log('Respuesta de la API:', response);
+      } else {
+        console.log('No se pudo enviar el curso.');
+      }
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('ingresado');
+    this.router.navigate(['/']); 
+  }
 }
+
